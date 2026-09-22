89cfc6ae4a9523c9ed12521cf060a1333d9a80fe
WiraRenn <rifadinwira@gmail.com>
2026-09-22 23:08:22 +0700

    posttest2

--- posttest/posttest-apd-2/2609106091-Muhammad_Wira_Rifadin_Noor-PT-2.py
+++ posttest/posttest-apd-2/2609106091-Muhammad_Wira_Rifadin_Noor-PT-2.py
@@ -6,10 +6,9 @@ merchandise_5 =90000
 merchandise_6 =120000
 bungkus_kado = 7500
 harga_merchandise = [merchandise_1,merchandise_2,merchandise_3,merchandise_4,merchandise_5,merchandise_6]
-total_harga = (harga_merchandise[0] + harga_merchandise[1] + harga_merchandise[2] + harga_merchandise[3] + harga_merchandise[4] + harga_merchandise[5])
-total_harga = total_harga + bungkus_kado
-
-kurs_usd = 17000
+total_harga_merchandise = (harga_merchandise[0] + harga_merchandise[1] + harga_merchandise[2] + harga_merchandise[3] + harga_merchandise[4] + harga_merchandise[5])
+total_harga = total_harga_merchandise + bungkus_kado
+kurs_usd = 17810
 total_usd = total_harga / kurs_usd
 
 rata_rata = total_harga / len(harga_merchandise)
@@ -22,7 +21,7 @@ slice_merchandise = harga_merchandise[-5:-2]
 print("Harga Merchandise :", harga_merchandise)
 print("Total Harga :", total_harga)
 print("Rata-rata :", rata_rata)
+print("Total dalam USD : $", total_usd)
 print("NIM :", nim)
 print("Bolean :", bolean)
-print("Total dalam USD : $", total_usd)
 print("Merchandise 2 hingga Merchandise 4 :", slice_merchandise)
